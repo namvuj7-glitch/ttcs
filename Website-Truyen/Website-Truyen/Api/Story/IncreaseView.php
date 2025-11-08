66d1c00d9ed3730e2bbb5adbcd52d4024236cf56
namvuj7-glitch <namvuj7@gmail.com>
2025-11-08 14:19:19 +0700

    Delete Website-Truyen/Website-Truyen directory

--- Website-Truyen/Website-Truyen/Api/Story/IncreaseView.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'Includes/Connects.php';
-
-$storyID = $_POST['StoryID'] ?? 0;
-$conn->query("UPDATE story SET view = view + 1 WHERE StoryID = $storyID");
-echo json_encode(['success' => true]);
-?>
